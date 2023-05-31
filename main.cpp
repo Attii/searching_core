43d23d972558ba4135e913822078905b92b6ede1
Attii <attij@yandex.ru>
2023-05-30 20:36:11 -0500

    Added 'using namespace std::string_literals;'

--- main.cpp
+++ main.cpp
@@ -7,24 +7,26 @@
 #include "search_server.h"
 #include "request_queue.h"
 
+using namespace std::string_literals;
+
 int main() {
-    SearchServer search_server("and in at");
+    SearchServer search_server("and in at"s);
     RequestQueue request_queue(search_server);
-    search_server.AddDocument(1, "curly cat curly tail", DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "curly dog and fancy collar", DocumentStatus::ACTUAL, {1, 2, 3});
-    search_server.AddDocument(3, "big cat fancy collar ", DocumentStatus::ACTUAL, {1, 2, 8});
-    search_server.AddDocument(4, "big dog sparrow Eugene", DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "big dog sparrow Vasiliy", DocumentStatus::ACTUAL, {1, 1, 1});
+    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
     // 1439 запросов с нулевым результатом
     for (int i = 0; i < 1439; ++i) {
-        request_queue.AddFindRequest("empty request");
+        request_queue.AddFindRequest("empty request"s);
     }
     // все еще 1439 запросов с нулевым результатом
-    request_queue.AddFindRequest("curly dog");
+    request_queue.AddFindRequest("curly dog"s);
     // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
-    request_queue.AddFindRequest("big collar");
+    request_queue.AddFindRequest("big collar"s);
     // первый запрос удален, 1437 запросов с нулевым результатом
-    request_queue.AddFindRequest("sparrow");
+    request_queue.AddFindRequest("sparrow"s);
     std::cout << "Total empty requests: " << request_queue.GetNoResultRequests() << std::endl;
     return 0;
 }
