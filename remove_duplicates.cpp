d7ca35cfc0b47bd79ce70120acf02d7903ef9424
Attii <attij@yandex.ru>
2023-08-08 17:17:33 -0500

    remove_duplicates.cpp algorithm changed

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -2,21 +2,19 @@
 
 void RemoveDuplicates(SearchServer& search_server) {
     std::set<int> duplicates;
-    
-    for (const int document_id : search_server) {
-        if (duplicates.count(document_id)) continue; 
-        const std::map<std::string, double> doc_to_comp = search_server.GetWordFrequencies(document_id);
+    std::set<std::set<std::string>> words_in_documents;
 
-        for (const int next_document_id : search_server) {
-            if(!duplicates.count(next_document_id) && next_document_id != document_id) {
-                const auto doc_to_comp2 = search_server.GetWordFrequencies(next_document_id);
-                if (doc_to_comp.size() == doc_to_comp2.size() && 
-                    std::equal(doc_to_comp.begin(), doc_to_comp.end(), doc_to_comp2.begin(), 
-                    [](auto a, auto b) { return a.first == b.first; })) 
-                {
-                    duplicates.insert(next_document_id);
-                }
-            }
+    for (const int document_id : search_server) {
+        std::set<std::string>  words_in_doc;
+
+        for (const auto& [word, freq] : search_server.GetWordFrequencies(document_id)) {
+            words_in_doc.insert(word);
+        }
+
+        if (words_in_documents.count(words_in_doc)) {
+            duplicates.insert(document_id);
+        } else {
+            words_in_documents.insert(words_in_doc);
         }
     }
 
